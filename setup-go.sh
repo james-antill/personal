dfd459fb2fa2fdc2068e61f8774bce99263075d0
James Antill <james@and.org>
2017-06-03 19:20:36 -0400

    Random go things.

--- setup-go.sh
+++ setup-go.sh
@@ -10,3 +10,15 @@ for i in uitime ; do
         fi
 done
 
+for i in github.com/anacrolix/torrent/cmd/torrent
+         github.com/a8m/tree/cmd/tree
+         github.com/cavaliercoder/grab/cmd/grab
+         github.com/bcicen/ctop
+         github.com/syncthing/syncthing
+         github.com/restic/restic/src/cmds/restic
+         ; do
+        if [ ! -d $i ]; then
+                go get github.com/james-antill/$i
+        fi
+done
+
