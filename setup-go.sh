cb5c150f0582ee6fc6697dbcb0d618879ba1eb46
James Antill <james@and.org>
2017-07-17 18:28:15 -0400

    Do updates, add go-pry

--- setup-go.sh
+++ setup-go.sh
@@ -6,19 +6,21 @@ fi
 
 for i in uitime ; do
         if [ ! -d $i ]; then
-                go get github.com/james-antill/$i
+                go get -u github.com/james-antill/$i
         fi
 done
 
-for i in github.com/anacrolix/torrent/cmd/torrent
-         github.com/a8m/tree/cmd/tree
-         github.com/cavaliercoder/grab/cmd/grab
-         github.com/bcicen/ctop
-         github.com/syncthing/syncthing
-         github.com/restic/restic/src/cmds/restic
+for i in github.com/anacrolix/torrent/cmd/torrent \
+         github.com/a8m/tree/cmd/tree \
+         github.com/cavaliercoder/grab/cmd/grab \
+         github.com/bcicen/ctop \
+         github.com/syncthing/syncthing \
+         github.com/restic/restic/src/cmds/restic \
+         github.com/d4l3k/go-pry \
          ; do
         if [ ! -d $i ]; then
-                go get github.com/james-antill/$i
+                echo $i
+                go get -u $i || true
         fi
 done
 
